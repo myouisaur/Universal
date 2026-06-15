17ba7f082ed3f8629807777c0ef736c6235324f6
myouisaur <myouisaur@gmail.com>
2026-06-15 18:41:20 +0800

    Bump version from 12.0 to 12.1

--- kmedia-downloader.user.js
+++ kmedia-downloader.user.js
@@ -2,7 +2,7 @@
 // @name         [Universal] K-Media Downloader
 // @namespace    https://github.com/myouisaur/Universal
 // @icon         data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23FF4081'%3E%3Cpath d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 11h3l-4 4-4-4h3V8h2v5z'/%3E%3C/svg%3E
-// @version      12.0
+// @version      12.1
 // @description  Organizes, tracks, and saves categorized K-Pop media files through a centralized overlay.
 // @author       Xiv
 // @match        *://*/*
@@ -17,6 +17,7 @@
 // @grant        window.close
 // @connect      raw.githubusercontent.com
 // @connect      kmedia-db-proxy.myouisaur.workers.dev
+// @connect      *
 // @noframes
 // @run-at       document-end
 // @updateURL    https://myouisaur.github.io/Universal/kmedia-downloader.user.js
