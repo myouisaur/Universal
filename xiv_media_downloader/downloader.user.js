295faf74beeac20c571774e6b2f54e6f57c1faaa
myouisaur <myouisaur@gmail.com>
2026-09-14 11:36:21 +0800

    Update version number to 33.1

--- xiv_media_downloader/downloader.user.js
+++ xiv_media_downloader/downloader.user.js
@@ -2,7 +2,7 @@
 // @name         [Universal] Xiv Media Downloader
 // @namespace    https://github.com/myouisaur/Universal
 // @icon         data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23FF4081'%3E%3Cpath d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 11h3l-4 4-4-4h3V8h2v5z'/%3E%3C/svg%3E
-// @version      33.0
+// @version      33.1
 // @description  Organizes, tracks, and saves categorized media files through a centralized overlay.
 // @author       Xiv
 // @match        *://*/*
